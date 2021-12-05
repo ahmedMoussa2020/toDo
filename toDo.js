7b070f1e0e3270d9758c0492f734595c86503571
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-12-05 18:23:45 +0000

    clean the cat

---
+++ toDo.js
@@ -0,0 +1,48 @@
+// Arrays to keep track of each task's state
+//const taskTitles = [];
+//const taskComplete = [];
+
+
+const newTask = (title, description) => {
+  let task = {
+    "title": title,
+    "description": description,
+    "complete": false,
+
+    markCompleted: function() {
+      this.complete = true;
+    },
+
+    logState: function() {
+      console.log(`${JSON.stringify(this.title)} has ${this.complete ? "" : "not"} been completed`);
+    }
+  };
+  return task;
+};
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+/*function newTask(title) {
+  taskTitles.push(title);
+  taskComplete.push(false);
+}
+
+// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+function completeTask(taskIndex) {
+  taskComplete[taskIndex] = true;
+}
+
+// Print the state of a task to the console in a nice readable way
+function logTaskState(taskIndex) {
+  const title = taskTitles[taskIndex];
+  const complete = taskComplete[taskIndex];
+  console.log(`${title} has${complete ? " " : " not "}been completed`);
+}*/
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter");// task 0
+const task2 = newTask("Do Laundry"); // task 1
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
